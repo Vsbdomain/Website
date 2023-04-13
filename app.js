31780b30a7cbe40a589d0bc7a6b8d56da7863d0f
skcreationz <selvakumard274@gmail,com>
2023-04-13 10:58:20 +0530

    New Update

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 //library dec
 const express=require('express');
+const ejs=require('ejs');
 const app=express();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
+// app.use(express.static("views"))
 //HTTP Request specs
 app.get("/",(req,res)=>{
-    res.render("dash_board");
+    res.render("header");
+})
+app.get("/dashboard",(req,res)=>{
+    res.render("dashboard");
+})
+app.get("/course",(req,res)=>{
+    res.render("course");
 })
 //port specification
 app.listen(3000,()=>{
