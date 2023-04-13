957aad0c40cf5970e2263901fead490410e68e4a
suryas03 <suryakgm7203@gmail.com>
2023-04-13 11:08:10 +0530

    update by surya

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //library dec
+const ejs= require("ejs")
 const express=require('express');
 const app=express();
 //middleware
@@ -7,7 +8,13 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 //HTTP Request specs
 app.get("/",(req,res)=>{
-    res.render("dash_board");
+    res.render("header");
+})
+app.get("/dashboard",(req,res)=>{
+    res.render("dashboard");
+})
+app.get("/course",(req,res)=>{
+    res.render("course");
 })
 //port specification
 app.listen(3000,()=>{
